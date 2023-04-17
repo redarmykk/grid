57dd068eaa9a270442610237f78a46c2fa3f62c6
Kirill <red.kirill.army@gmail.com>
2023-04-17 19:45:51 +0600

    grid

---
+++ js/burger.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#burger').addEventListener('click', function() {
+    document.querySelector('#menu').classList.toggle('is-active')
+  })
+})
